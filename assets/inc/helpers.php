7b730cd6923426a21cae96a2509a9e981dcf0755
Krimo Hafsaoui <krimo@pixelsmith.co>
2016-09-20 15:32:39 -0500

    dir structure

---
+++ assets/inc/helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+* Helper Class: package our methods
+*/
+class Helpers
+{
+	
+	function __construct()
+	{
+	}
+
+	static public function slugify($text)
+	{
+		$text = preg_replace('~[^\pL\d]+~u', '-', $text);
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+		$text = preg_replace('~[^-\w]+~', '', $text);
+		$text = trim($text, '-');
+		$text = preg_replace('~-+~', '-', $text);
+		$text = strtolower($text);
+
+		if (empty($text)) {
+			return 'n-a';
+		}
+
+		return $text;
+	}
+
+	
+}
---
+++ gruntfile.js
@@ -0,0 +1,146 @@
+module.exports = function(grunt) {
+
+	var globalConfig = {
+		proxy: 'dependencies.dev'
+	};
+
+	// load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+
+	grunt.initConfig({
+		globalConfig: globalConfig,
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			options: {
+				sourceMap: true
+			},
+
+			dist: {
+				options: {
+					outputStyle: 'compressed'
+				},
+				files: {
+					'assets/css/styles.min.css': 'assets/scss/styles.scss',
+				}
+			}
+		},
+
+		scsslint: {
+			allFiles: [
+			'assets/scss/*.scss'
+			],
+			options: {
+				config: '.scss-lint.yml',
+				colorizeOutput: true
+			},
+		},
+
+		postcss: {
+			options: {
+				processors: [
+				require('pixrem')(),
+				require('autoprefixer')({browsers: ['last 2 versions', 'ie >= 9']}),
+				]
+			},
+			dist: {
+				src: 'assets/css/styles.min.css'
+			}
+		},
+
+		concat: {
+			options: {
+				separator: ';',
+			},
+			dist: {
+				src: [
+					'assets/js/modules/theme.js'
+				],
+				dest: 'assets/js/modules.js',
+			},
+		},
+
+		uglify: {
+			dist: {
+				files: {
+					'assets/js/dist/main.min.js': ['assets/js/modules.js']
+				}
+			}
+		},
+
+		jshint: {
+			all: ['Gruntfile.js', 'assets/js/modules/*.js'],
+
+			options: {
+				reporter: require('jshint-stylish'),
+				jshintrc: '.jshintrc'
+			}
+		},
+
+		phplint: {
+			all: ['**/*.php', '!vendor/*.php', '!vendor/**/*.php']
+		},
+
+		githooks: {
+			options: {
+				hashbang: '#!/bin/sh',
+				template: 'node_modules/grunt-githooks/templates/shell.hb',
+				startMarker: '## GRUNT-GITHOOKS START',
+				endMarker: '## GRUNT-GITHOOKS END',
+				command: 'PATH='+process.env.PATH+' grunt'
+			},
+			all: {
+				'pre-commit': 'linting'
+			}
+		},
+
+		browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        '**/*.php', '!vendor/*.php', '!vendor/**/*.php',
+                        'assets/css/*.css',
+                        'assets/js/dist/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    proxy: '<%= globalConfig.proxy %>'
+                }
+            }
+        },
+
+		watch: {
+			grunt: { files: ['Gruntfile.js'] },
+
+			sass: {
+				files: 'assets/scss/**/*.scss',
+				tasks: ['newer:scsslint', 'sass', 'postcss'],
+				options: {
+					livereload:true,
+				}
+			},
+
+			js: {
+				files: ['assets/js/vendor/**/*.js', 'assets/js/modules/**/*.js', 'assets/js/main.js'],
+				tasks: ['concat', 'newer:uglify'],
+				options: {
+					livereload:true,
+				}
+			},
+
+			all: {
+				files: ['**/*.php', '!vendor/*.php', '!vendor/**/*.php'],
+				tasks: ['phplint'],
+				options: {
+					livereload:true,
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('build', ['scsslint', 'sass', 'postcss', 'jshint', 'concat', 'uglify']);
+	grunt.registerTask('linting', ['newer:scsslint', 'newer:jshint', 'newer:phplint']);
+	grunt.registerTask('default', ['browserSync', 'watch']);
+};
